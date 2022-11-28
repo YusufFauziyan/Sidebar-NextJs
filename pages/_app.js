962a24c71a6c50886ce36a219719d086fdf3835d
YusufFauziyan <yusuffauziyan@gmail.com>
2022-11-29 03:45:24 +0700

    commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import NewSidebar from "../components/sidebar";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  // return <Component {...pageProps} />
+  return <NewSidebar />;
+}
+
+export default MyApp;
